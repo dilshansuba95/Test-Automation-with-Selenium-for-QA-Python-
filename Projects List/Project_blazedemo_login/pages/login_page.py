8bad98ea2cebb462a9827c4db2ae97eec7b6070e
Dilshan Subasinghe <dilshansubasinghe99@gmail.com>
2024-08-14 14:06:42 +0530

    POM for the login is implemented

--- Projects List/Project_blazedemo_login/pages/login_page.py
+++ Projects List/Project_blazedemo_login/pages/login_page.py
@@ -12,17 +12,22 @@ class LoginPage:
         self.error_message = (By.CSS_SELECTOR, "body > div > div.message")
 
     def enter_email(self, email):
+        # Implement logic to enter email
         self.driver.find_element(*self.email_input).send_keys(email)
 
     def enter_password(self, password):
+        # Implement logic to enter password
         self.driver.find_element(*self.password_input).send_keys(password)
 
     def click_remember_me(self):
+        # Implement logic to click remember me checkbox
         self.driver.find_element(*self.remember_me_checkbox).click()
 
     def click_login(self):
+        # Implement logic to click the login button
         self.driver.find_element(*self.login_button).click()
         time.sleep(4)
 
     def get_error_message(self):
+        # Implement logic to retrieve error message
         return self.driver.find_element(*self.error_message).text
