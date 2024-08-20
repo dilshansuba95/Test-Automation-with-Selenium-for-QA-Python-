07ae2e6fd9b3d62eb71e46b79f1c4f0c2c1e28b2
Dilshan Subasinghe <dilshansubasinghe99@gmail.com>
2024-08-20 15:14:26 +0530

    POM is updated for empty fields scenario validation

--- Projects List/Project_login_test_orangeHRM/pages/login_page_orange.py
+++ Projects List/Project_login_test_orangeHRM/pages/login_page_orange.py
@@ -8,13 +8,23 @@ class LoginPage_orange():
         self.password_input = (By.XPATH,"//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")
         self.login_button = (By.XPATH,"//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]/button")
         self.error_msg = (By.XPATH,"//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/div/div/div[1]/p")
+        self.username_error = (By.XPATH,"//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/span")
+        self.password_error = (By.XPATH,"//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/span")
 
     def enter_username(self, username):  
         self.driver.find_element(*self.username_input).send_keys(username)
         time.sleep(1)
 
+    def enter_empty_username(self):  
+        self.driver.find_element(*self.username_input).clear()
+        time.sleep(1)
+
     def enter_password(self, password):
         self.driver.find_element(*self.password_input).send_keys(password)
+        time.sleep(1)
+
+    def enter_empty_password(self):
+        self.driver.find_element(*self.password_input).clear()
         time.sleep(1)
 
     def click_login_button(self):
@@ -23,4 +33,11 @@ class LoginPage_orange():
         
     def get_error_msg(self):
         return self.driver.find_element(*self.error_msg).text
+    
+    def get_empty_username_error_msg(self):
+        return self.driver.find_element(*self.username_error).text
+    
+    def get_empty_password_error_msg(self):
+        return self.driver.find_element(*self.password_error).text
+    
     
